39149e3a867b49f56db37eb6920b22c20ec18a73
Tisha Saha <tishasaha32@gmail.com>
2023-06-09 16:11:38 +0530

    Responsiveness

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Bar} from 'react-chartjs-2'
 import { Chart as ChartJS } from "chart.js/auto";
+import styles from './Chart.module.css'
 
 function Chart({ transactions }) {
   const [transactionChart, setTransactionChart] = useState(null);
@@ -35,9 +36,9 @@ function Chart({ transactions }) {
   }, [transactions]);
 
   return (
-    <div style={{width:'100vw', height: '60vh', padding: '20px'}}>
+    <div className={styles.chartContainer}>
       {transactionChart && (
-        <Bar
+        <Bar className={styles.chart} 
           data={transactionChart}
           options={{
             responsive: true,
